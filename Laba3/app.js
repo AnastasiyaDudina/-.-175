8f838da5ddbefb172af22e70030e59d35af95968
Анастасия Дудина <anastasiya.dudina@ms11.ru>
2020-05-04 18:40:10 +0500

    Laba 3

--- Laba3/app.js
+++ Laba3/app.js
@@ -43,57 +43,155 @@ app.get("/", (req, res)=>
 });
 
 
-app.get('/abc', function tt (req, res) {
-    var a = req.query.a;
-    var b = req.query.b;
-    var c = req.query.c;
-    var D = b*b-4*(a*c);
-    if (D>0) {
-        var x1 = (-1 * b - (Math.sqrt(D))) / 2 * a;
-        var x2 = (-1 * b + (Math.sqrt(D))) / 2 * a;
-        res.send({x1:x1, x2:x2});
-    }
-    else{
-        if (D === 0) {
-            var x = (-1 * b) / 2 * a;
-            res.send({x:x});
-        }
-        else{
-            res.send("действительных корней нет");
-        }
-    }
+Deal.findById = (dealId, result) => {
+sql.query(`SELECT * FROM TODO WHERE id = ${dealId}`, (err, res) => {
+if (err) {
+console.log(«error: », err);
+result(err, null);
+return;
+}
 
+if (res.length) {
+console.log(«найдено дело: », res[0]);
+result(null, res[0]);
+return;
+}
+
+// когда ничего не удалось найти
+result({ kind: «not_found» }, null);
 });
+};
 
-//No. 3
-app.get('/day', function (req, res) {
-    var date = req.query.date;
-    date = new Date(date);
-    var day = date.getDay();
-    var days = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
-    res.send({
-        days: days[day]
+Deal.getAll = result => {
+sql.query(«SELECT * FROM TODO», (err, res) => {
+if (err) {
+console.log(«error: », err);
+result(null, err);
+return;
+}
+
+console.log(«deals: », res);
+result(null, res);
+});
+};
+
+Deal.updateById = (id, deal, result) => {
+sql.query(
+«UPDATE TODO SET text =? WHERE id = ?»,
+[deal.text, id],
+(err, res) => {
+if (err) {
+console.log(«error: », err);
+result(null, err);
+return;
+}
+
+if (res.affectedRows == 0) {
+result({ kind: «not_found» }, null);
+return;
+}
+
+console.log(«Обновлено дело », { id: id, ...deal });
+result(null, { id: id, ...deal });
+}
+);
+};
+
+Deal.remove = (id, result) => {
+sql.query(«DELETE FROM TODO WHERE id = ?», id, (err, res) => {
+if (err) {
+console.log(«error: », err);
+result(null, err);
+return;
+}
+
+if (res.affectedRows == 0) {
+// если дело не удалось получить по id
+result({ kind: «not_found» }, null);
+return;
+}
+console.log(«Удален пользователь с », id);
+result(null, res);
+});
+};
+
+Deal.removeAll = result => {
+sql.query(«DELETE FROM TODO», (err, res) => {
+if (err) {
+console.log(«error: », err);
+result(null, err);
+return;
+}
+
+console.log(`deleted ${res.affectedRows} deals`);
+result(null, res);
+});
+};
+
+const pool = require('../data/config');
+
+// Route the app
+const router = app => {
+    // Display welcome message on the root
+    app.get('/', (request, response) => {
+        response.send({
+            message: 'Welcome to the Node.js Express REST API!'
+        });
     });
-});
 
+    // Display all users
+    app.get('/users', (request, response) => {
+        pool.query('SELECT * FROM users', (error, result) => {
+            if (error) throw error;
 
- //NO. 4
- app.get("/fib/:num", function (req, res) {
-    var n = req.params.num;
-    var a = 1,
-        b = 1;
-    for (let i = 3; i <= n; i++) {
-        var temp = a + b;
-        a = b;
-        b = temp;
-     };
-     res.send({
-         "число Фибоначчи": b
-     });
- });
+            response.send(result);
+        });
+    });
 
+    // Display a single user by ID
+    app.get('/users/:id', (request, response) => {
+        const id = request.params.id;
 
+        pool.query('SELECT * FROM users WHERE id = ?', id, (error, result) => {
+            if (error) throw error;
 
+            response.send(result);
+        });
+    });
+
+    // Add a new user
+    app.post('/users', (request, response) => {
+        pool.query('INSERT INTO users SET ?', request.body, (error, result) => {
+            if (error) throw error;
+
+            response.status(201).send(`User added with ID: ${result.insertId}`);
+        });
+    });
+
+    // Update an existing user
+    app.put('/users/:id', (request, response) => {
+        const id = request.params.id;
+
+        pool.query('UPDATE users SET ? WHERE id = ?', [request.body, id], (error, result) => {
+            if (error) throw error;
+
+            response.send('User updated successfully.');
+        });
+    });
+
+    // Delete a user
+    app.delete('/users/:id', (request, response) => {
+        const id = request.params.id;
+
+        pool.query('DELETE FROM users WHERE id = ?', id, (error, result) => {
+            if (error) throw error;
+            response.send('User deleted.');
+        });
+    });
+}
+
+// Export the router
+module.exports = router;
 // Start the server
 var server = app.listen(port, error => {
   if (error) return console.log(`Error: ${error}`);
